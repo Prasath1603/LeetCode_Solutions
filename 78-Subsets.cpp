110cb9e3afb417b7be8ffe56ad2c0e78860ff4c9
Malliprasath S <141343355+Prasath1603@users.noreply.github.com>
2024-10-18 11:20:09 +0530

    [78] [Time Beats: 100.00%] [Memory Beats: 90.29%] - LeetPush

--- 78-Subsets.cpp
+++ 78-Subsets.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     void powerSet(vector<vector<int>> &result,vector<int> &subset , int index , vector<int>& nums){
 
-        if(index == nums.size()){
-            result.push_back(subset);
-            return;
-        }
-        
-        powerSet(result , subset , index + 1 , nums);
-        subset.push_back(nums[index]);
+        result.push_back(subset);
 
-        powerSet(result , subset , index + 1 , nums);
-        subset.pop_back();
+        for(int i = index ; i < nums.size() ; i++){
+
+            subset.push_back(nums[i]);
+
+            powerSet(result , subset , i + 1 , nums);
+
+            subset.pop_back();
+        }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
