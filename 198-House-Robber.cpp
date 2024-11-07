3fd6c367ffd2eb88d726998b5bc417ee01c65e0e
Malliprasath S <141343355+Prasath1603@users.noreply.github.com>
2024-11-07 14:02:55 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 12.13%] - LeetPush

--- 198-House-Robber.cpp
+++ 198-House-Robber.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int House(vector<int> &nums,int curr,vector<int>& dp){
-        if(curr >= nums.size()) return 0;
-
-        if(dp[curr] != -1) return dp[curr];
-        int take = nums[curr] + House(nums, curr + 2,dp);
-        int notTake = House(nums, curr + 1,dp);
-
-        return dp[curr] = max(take, notTake);
-    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size() , -1);
-        return House(nums, 0, dp);
+        int n = nums.size(); 
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        vector<int> dp(n , 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1],nums[0]);
+        for(int i = 2 ; i < n ; i++){
+            dp[i] = max(dp[i - 1] , nums[i] + dp[i - 2]);
+        }
+        return dp[n - 1];
     }
 };
